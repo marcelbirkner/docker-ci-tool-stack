b575df62cfdec775162b4263e4b5dade5f7f7d8a
Marcel Birkner <marcel_birkner-extern@gothaer-systems.de>
2016-04-08 18:00:13 +0200

    refactored conference app seed job

--- jenkins/seedjobs/conferenceAppSeedJob.groovy
+++ jenkins/seedjobs/conferenceAppSeedJob.groovy
@@ -1,170 +1,97 @@
 def giturl = 'https://github.com/codecentric/conference-app'
-job("conference-app-1-ci") {
-  parameters {
-    stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
-    stringParam("REPOSITORY_URL", "http://nexus:8081/nexus/content/repositories/releases/", "Nexus Release Repository URL")
-  }
-  scm {
-    git {
-      remote {
-        url(giturl)
-      }
-      createTag(false)
-      clean()
+
+createCiJob("conference-app", gitUrl, "app/pom.xml")
+createSonarJob("conference-app", gitUrl, "app/pom.xml")
+
+createCiJob("conference-app-monitoring", gitUrl, "monitoring/pom.xml")
+createSonarJob("conference-app-monitoring", gitUrl, "monitoring/pom.xml")
+
+def createCiJob(def jobName, def gitUrl, def pomFile) {
+  job("${jobName}-1-ci") {
+    parameters {
+      stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
+      stringParam("REPOSITORY_URL", "http://nexus:8081/nexus/content/repositories/releases/", "Nexus Release Repository URL")
     }
-  }
-  wrappers {
-    colorizeOutput()
-    preBuildCleanup()
-  }
-  triggers {
-    scm('30/H * * * *')
-    githubPush()
-  }
-  steps {
-    maven {
-        goals('clean versions:set -DnewVersion=DEV-\${BUILD_NUMBER}')
+    scm {
+      git {
+        remote {
+          url(giturl)
+        }
+        createTag(false)
+        clean()
+      }
+    }
+    wrappers {
+      colorizeOutput()
+      preBuildCleanup()
+    }
+    triggers {
+      scm('30/H * * * *')
+      githubPush()
+    }
+    steps {
+      maven {
+          goals('clean versions:set -DnewVersion=DEV-\${BUILD_NUMBER}')
+          mavenInstallation('Maven 3.3.3')
+          rootPOM( pomFile )
+          mavenOpts('-Xms512m -Xmx1024m')
+          providedGlobalSettings('MyGlobalSettings')
+      }
+      maven {
+        goals('clean deploy')
         mavenInstallation('Maven 3.3.3')
-        rootPOM('app/pom.xml')
+        rootPOM(pomFile)
         mavenOpts('-Xms512m -Xmx1024m')
         providedGlobalSettings('MyGlobalSettings')
+      }
     }
-    maven {
-      goals('clean deploy')
-      mavenInstallation('Maven 3.3.3')
-      rootPOM('app/pom.xml')
-      mavenOpts('-Xms512m -Xmx1024m')
-      providedGlobalSettings('MyGlobalSettings')
-    }
-  }
-  publishers {
-    chucknorris()
-    archiveJunit('**/target/surefire-reports/*.xml')
-    publishCloneWorkspace('**', '', 'Any', 'TAR', true, null)
-    downstreamParameterized {
-      trigger('conference-app-2-sonar') {
-        currentBuild()
+    publishers {
+      chucknorris()
+      archiveJunit('**/target/surefire-reports/*.xml')
+      publishCloneWorkspace('**', '', 'Any', 'TAR', true, null)
+      downstreamParameterized {
+        trigger("${jobName}-2-sonar") {
+          currentBuild()
+        }
       }
     }
   }
 }
-job("conference-app-2-sonar") {
-  parameters {
-    stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
-  }
-  scm {
-    cloneWorkspace("conference-app-1-ci")
-  }
-  wrappers {
-    colorizeOutput()
-    preBuildCleanup()
-  }
-  steps {
-    maven {
-      goals('org.jacoco:jacoco-maven-plugin:0.7.4.201502262128:prepare-agent install -Psonar')
-      mavenInstallation('Maven 3.3.3')
-      rootPOM('app/pom.xml')
-      mavenOpts('-Xms512m -Xmx1024m')
-      providedGlobalSettings('MyGlobalSettings')
+
+def createSonarJob(def jobName, def gitUrl, def pomFile) {
+  job("${jobName}-2-sonar") {
+    parameters {
+      stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
     }
-    maven {
-      goals('sonar:sonar -Psonar')
-      mavenInstallation('Maven 3.3.3')
-      rootPOM('app/pom.xml')
-      mavenOpts('-Xms512m -Xmx1024m')
-      providedGlobalSettings('MyGlobalSettings')
+    scm {
+      cloneWorkspace("${jobName}-1-ci")
     }
-  }
-  publishers {
-    chucknorris()
-  }
-}
-job("conference-app-monitoring-1-ci") {
-  parameters {
-    stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
-    stringParam("REPOSITORY_URL", "http://nexus:8081/nexus/content/repositories/releases/", "Nexus Release Repository URL")
-  }
-  scm {
-    git {
-      remote {
-        url(giturl)
-      }
-      createTag(false)
-      clean()
+    wrappers {
+      colorizeOutput()
+      preBuildCleanup()
     }
-  }
-  wrappers {
-    colorizeOutput()
-    preBuildCleanup()
-  }
-  triggers {
-    scm('30/H * * * *')
-    githubPush()
-  }
-  steps {
-    maven {
-        goals('clean versions:set -DnewVersion=DEV-\${BUILD_NUMBER}')
+    steps {
+      maven {
+        goals('org.jacoco:jacoco-maven-plugin:0.7.4.201502262128:prepare-agent install -Psonar')
         mavenInstallation('Maven 3.3.3')
-        rootPOM('monitoring/pom.xml')
+        rootPOM(pomFile)
         mavenOpts('-Xms512m -Xmx1024m')
         providedGlobalSettings('MyGlobalSettings')
-    }
-    maven {
-      goals('clean deploy -DaltDeploymentRepository=nexus-release-repository::default::$REPOSITORY_URL')
-      mavenInstallation('Maven 3.3.3')
-      rootPOM('monitoring/pom.xml')
-      mavenOpts('-Xms512m -Xmx1024m')
-      providedGlobalSettings('MyGlobalSettings')
-    }
-  }
-  publishers {
-    chucknorris()
-    publishCloneWorkspace('**', '', 'Any', 'TAR', true, null)
-    downstreamParameterized {
-      trigger('conference-app-monitoring-2-sonar') {
-        currentBuild()
       }
-    }
-  }
-}
-job("conference-app-monitoring-2-sonar") {
-  parameters {
-    stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
-  }
-  scm {
-    cloneWorkspace("conference-app-monitoring-1-ci")
-  }
-  wrappers {
-    colorizeOutput()
-    preBuildCleanup()
-  }
-  steps {
-    maven {
-        goals('clean versions:set -DnewVersion=DEV-\${BUILD_NUMBER}')
+      maven {
+        goals('sonar:sonar -Psonar')
         mavenInstallation('Maven 3.3.3')
-        rootPOM('monitoring/pom.xml')
+        rootPOM(pomFile)
         mavenOpts('-Xms512m -Xmx1024m')
         providedGlobalSettings('MyGlobalSettings')
+      }
     }
-    maven {
-      goals('org.jacoco:jacoco-maven-plugin:0.7.4.201502262128:prepare-agent install -Psonar')
-      mavenInstallation('Maven 3.3.3')
-      rootPOM('monitoring/pom.xml')
-      mavenOpts('-Xms512m -Xmx1024m')
-      providedGlobalSettings('MyGlobalSettings')
+    publishers {
+      chucknorris()
     }
-    maven {
-      goals('sonar:sonar -Psonar')
-      mavenInstallation('Maven 3.3.3')
-      rootPOM('monitoring/pom.xml')
-      mavenOpts('-Xms512m -Xmx1024m')
-      providedGlobalSettings('MyGlobalSettings')
-    }
-  }
-  publishers {
-    chucknorris()
   }
 }
+
 listView('Conference App') {
     description('')
     filterBuildQueue()
@@ -182,6 +109,51 @@ listView('Conference App') {
         lastDuration()
     }
 }
+
+def conferenceAppGitUrl="https://github.com/codecentric/conference-app"
+createDockerJob("conference-app-build-container", "cd app && sudo /usr/bin/docker build -t conferenceapp .", conferenceAppGitUrl)
+createDockerJob("conference-app-start-container", "sudo /usr/bin/docker run -d --name conferenceapp -p=48080:8080 conferenceapp", conferenceAppGitUrl)
+createDockerJob("conference-app-stop-container", 'sudo /usr/bin/docker stop \$(sudo /usr/bin/docker ps -a -q --filter="name=conferenceapp") && sudo /usr/bin/docker rm \$(sudo /usr/bin/docker ps -a -q --filter="name=conferenceapp")', " ")
+
+def createDockerJob(def jobName, def shellCommand, def gitRepository) {
+
+  println "############################################################################################################"
+  println "Creating Docker Job ${jobName} for gitRepository=${gitRepository}"
+  println "############################################################################################################"
+
+  job(jobName) {
+    logRotator {
+        numToKeep(10)
+    }
+    if( "${gitRepository}".size() > 0 ) {
+      if( "${jobName}".contains("conference-app") ) {
+        scm {
+          cloneWorkspace("conference-app-1-ci")
+        }
+      } else {
+        scm {
+          git {
+            remote {
+              url(gitRepository)
+            }
+            createTag(false)
+            clean()
+          }
+        }
+      }
+    }
+    steps {
+      steps {
+        shell(shellCommand)
+      }
+    }
+    publishers {
+      chucknorris()
+    }
+  }
+}
+
+
 listView('Conference App Docker') {
     description('')
     filterBuildQueue()
